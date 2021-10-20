2a795d3710ce091367d795e138decdb0b277292a
wuyuzhen11 <970388658@qq.com>
2021-10-20 11:36:43 +0800

    home商品展示列表

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,26 +5,45 @@
     </nav-bar>
     <home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view :goods="goods.pop.list"/>
+    <tab-control class="tab-control"
+                 :titles="['流行','新款','精选']"
+                 @tabClick="tabClick"
+    />
+    <goods-list :goods="goods[currentType].list"/>
   </div>
 </template>
 
 <script>
-  import NavBar from "components/common/navbar/NavBar";
   import HomeSwiper from "./childComps/HomeSwiper";
-  import {getHomeMultidata} from "network/home";
+  import FeatureView from "./childComps/FeatureView";
   import RecommendView from "./childComps/RecommendView";
 
+  import NavBar from "components/common/navbar/NavBar";
+  import TabControl from "components/content/tabControl/TabControl";
+  import GoodsList from "components/content/goods/GoodsList";
+
+  import {getHomeMultidata,getHomeGoods} from "network/home";
   export default {
     name: "Home",
     components:{
       HomeSwiper,
       NavBar,
       RecommendView,
+      FeatureView,
+      TabControl,
+      GoodsList
     },
     data(){
       return{
         banners:[],
-        recommends:[]
+        recommends:[],
+        goods:{
+          'pop':{page:0,list:[]},
+          'new':{page:0,list:[]},
+          'sell':{page:0,list:[]},
+        },
+        currentType:'pop'
       }
     },
     created() {
@@ -34,13 +53,59 @@
         this.banners=res.data.banner.list
         this.recommends=res.data.recommend.list
       })
+      //请求商品数据
+      this.getHomeGoods('pop')
+      this.getHomeGoods('new')
+      this.getHomeGoods('sell')
+    },
+    methods:{
+      /**
+       * 事件监听
+       */
+      tabClick(index){
+        switch (index){
+          case 0:
+            this.currentType='pop'
+            break
+          case 1:
+            this.currentType='new'
+            break
+          case 2:
+            this.currentType='sell'
+            break
+        }
+      },
+      /**
+       * 网络请求
+       * @param type
+       */
+      getHomeGoods(type){
+        const page = this.goods[type].page+1
+        getHomeGoods(type,page).then(res=>{
+          this.goods[type].list.push(...res.data.list)
+          this.goods[type].page+=1
+        })
+      }
     }
   }
 </script>
 
 <style scoped>
-.home-nav{
-  background-color: var(--color-tint);
-  color: #fff;
-}
+  #home{
+    padding-top: 44px;
+  }
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control{
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
 </style>
